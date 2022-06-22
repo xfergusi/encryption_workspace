2617e60443949a0962a7ee660e37fcf5bb5b98b6
Ian Ferguson <ian@IanFergusonsAir.localdomain>
2022-06-22 13:03:11 +1000

    getting ready for total handover

--- decrypter.py
+++ decrypter.py
@@ -9,21 +9,21 @@ BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
  
-private_key = bytearray.fromhex("16691a3ad4b2449aa0895bf1768a3b9cfbae3c24992df7482cc215dbb9881330")
+#hex key passed from js script
+private_key = bytearray.fromhex("6cba96607e146a0015b8fd703f7d15375370d6a81d55bb80fae57b89ef68d765")
  
 
 def encrypt(raw):
     raw = pad(raw)
-    # iv = Random.new().read(AES.block_size)
-    iv = bytes.fromhex("72de0ba2966617fdda284a8d99f3ab9d")
+    # iv = Random.new().read(AES.block_size)   <==== How you would create a random IV, I used the same one from the JS script.
+    iv = bytes.fromhex("504ce4bd4ed41d7ac97018a3da211fd4")
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    # return base64.b64encode(iv + cipher.encrypt(raw.encode("utf8")))
-    print((cipher.encrypt(raw.encode("utf8"))).hex())
-    return base64.b64encode(cipher.encrypt(raw.encode("utf8")))
+    print((cipher.encrypt(raw.encode("utf8"))).hex()) # given the same key and IV, we should get the same hex encrypted message
+    return base64.b64encode(iv + cipher.encrypt(raw.encode())) # This is the way that they pass the IV to the decrypt method
  
  
 def decrypt(enc):
-    # enc = base64.b64decode(enc)
+    enc = base64.b64decode(enc)
     iv = enc[:16]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(enc[16:]))
--- test.js
+++ test.js
@@ -1,10 +1,12 @@
+//part 1: making DiffieHellman keys
 const assert = require('node:assert');
 
 const {
   createDiffieHellman,
 } = require('node:crypto');
 
-// Generate Alice's keys...
+// Generate Alice's keys... I chose to do a 256 sized key because that's the size needed for aes 256 enc. 
+// BUT, if you want to create a large key then sha 256 it, that would work as well.
 const alice = createDiffieHellman(256);
 const aliceKey = alice.generateKeys();
 
@@ -16,9 +18,11 @@ const bobKey = bob.generateKeys();
 const aliceSecret = alice.computeSecret(bobKey);
 const bobSecret = bob.computeSecret(aliceKey);
 
-// OK
+// OK, this shows that they are the same key! congrats, we did it
 assert.strictEqual(aliceSecret.toString('hex'), bobSecret.toString('hex'));
 
+
+// Part 2: using the key to encrypt something.
 const crypto = require("crypto");
 
 const algorithm = "aes-256-cbc"; 
@@ -29,6 +33,7 @@ const initVector = crypto.randomBytes(16);
 // protected data
 const message = "testing attention please";
 
+//This is the information I copied over to the other scripts to create the same encrypted string
 console.log("initVector: " + initVector.toString('hex'))
 console.log("secret in hex: " + aliceSecret.toString('hex'));
 
@@ -42,6 +47,8 @@ let encryptedData = cipher.update(message, "utf-8", "hex");
 encryptedData += cipher.final("hex");
 console.log("Encrypted message: " + encryptedData);
 
+
+//Part 3: Decrypting that message
 const decipher = crypto.createDecipheriv(algorithm, aliceSecret, initVector);
 
 let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
